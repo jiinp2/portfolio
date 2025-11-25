eb020295cdeb8805a9ed1ddb2256a705908a8ce0
jiinp2 <jiinpark.work@gmail.com>
2025-11-25 12:12:15 -0500

    Fix build error

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Work from './pages/Work'
 import Misc from './pages/Misc'
-import InfoCardTest from './pages/InfoCardTest'
 import './App.css'
 
 function App() {
@@ -11,7 +10,6 @@ function App() {
         <Routes>
           <Route path="/" element={<Work />} />
           <Route path="/misc" element={<Misc />} />
-          <Route path="/test" element={<InfoCardTest />} />
           <Route path="/:projectSlug" element={<Work />} />
         </Routes>
       </div>
